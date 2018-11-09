25a3134f9e737dacca2426163f2ef38d276b6d91
Cody Pitts <cpitts@forumone.com>
2018-11-08 17:03:37 -0800

    Added query parameter functionality to the api complaint list

--- api/views.py
+++ api/views.py
@@ -23,3 +23,24 @@ class ComplaintViewSet(viewsets.ModelViewSet):
 
     def perform_create(self, serializer):
         serializer.save(owner=self.request.user)
+
+    """
+    List a queryset, but adding some querystring functionality.
+    """
+    def list(self, request, *args, **kwargs):
+        queryset = self.filter_queryset(self.get_queryset())
+
+        if request.GET.get('filter_type_map', ''):
+            queryset = queryset.filter(record_type_map=request.GET.get('filter_type_map'))
+        if request.GET.get('sort', '').lower() == 'asc':
+            queryset = queryset.order_by('open_date')
+        if request.GET.get('sort', '').lower() == 'desc':
+            queryset = queryset.order_by('-open_date')
+
+        page = self.paginate_queryset(queryset)
+        if page is not None:
+            serializer = self.get_serializer(page, many=True)
+            return self.get_paginated_response(serializer.data)
+
+        serializer = self.get_serializer(queryset, many=True)
+        return Response(serializer.data)
